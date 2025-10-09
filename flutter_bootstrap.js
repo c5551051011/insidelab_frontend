ebb95f1929fe116de1f41227b61398b61406cb2f
c5551051011 <c5551051011@users.noreply.github.com>
2025-10-09 07:24:55 +0000

    deploy: 50b0bc2cfefaff71da6628ffa35f3d91f668fba0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1404227019"
+    serviceWorkerVersion: "1206418740"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -99,9 +99,9 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "CNAME": "eac243177ebbc54b7abf06067174ab0d",
 "404.html": "5cd7d08f8672882aa60b2030b4b29d43",
-"flutter_bootstrap.js": "516c8b83f70305a25052870799994531",
+"flutter_bootstrap.js": "e7911b38a4092a60df666fe0e8a9a86b",
 "version.json": "54140ec5237bca28e40b658f700695ae",
-"main.dart.js": "d560f54460dd19472aedb9daca7c72bf"};
+"main.dart.js": "a658032df45ca941b531dc9176e990b6"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
